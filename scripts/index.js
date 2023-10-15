bdf3c5e10a0cbeb48b2f94a7d865b3fcf4bf5c62
Olga <olaliss016@gmail.com>
2023-10-15 10:14:11 +0300

    сдача1.0

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,32 @@
 // @todo: Темплейт карточки
+const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
+const cardContainer = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
+function createCard(cardData, handleDeleteClick) {
+    const cardElement = cardTemplate.querySelector('.places__item').cloneNode(true);
+    
+    cardElement.querySelector('.card__title').textContent = cardData.name;
+    const cardImage = cardElement.querySelector('.card__image');
+    cardImage.src = cardData.link;
+    cardImage.alt = cardData.name;
+    
+    const deleteButton = cardElement.querySelector('.card__delete-button');
+    deleteButton.addEventListener('click', () => handleDeleteClick(cardElement));
+    
+    return cardElement;
+  }
 
 // @todo: Функция удаления карточки
+function handleDeleteClick(cardElement) {
+    cardElement.remove();
+  }
 
 // @todo: Вывести карточки на страницу
+
+initialCards.forEach((cardData) => {
+  const cardElement = createCard(cardData, handleDeleteClick);
+  cardContainer.append(cardElement);
+});
